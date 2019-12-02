8d833b9f65f2618ba5532d6125ae73439dc37c39
markus.rohsto@gmail.com <markus.rohsto@gmail.com>
2019-12-02 21:38:54 +0100

    kaka

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -679,7 +679,7 @@
 
   #if ENABLED(DELTA_AUTO_CALIBRATION) || ENABLED(DELTA_CALIBRATION_MENU)
     // Set the radius for the calibration probe points - max DELTA_PRINTABLE_RADIUS for non-eccentric probes
-    #define DELTA_CALIBRATION_RADIUS DELTA_PRINTABLE_RADIUS - MIN_PROBE_EDGE  // (mm)
+    //#define DELTA_CALIBRATION_RADIUS DELTA_PRINTABLE_RADIUS - MIN_PROBE_EDGE  // (mm)
     // Set the steprate for papertest probing
     #define PROBE_MANUALLY_STEP 0.05      // (mm)
   #endif
